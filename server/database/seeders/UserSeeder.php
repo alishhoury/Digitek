43c8da0271be752d173daf1f530e9c5df71f3e3a
Ali AlSaghir <alisalsaghir@gmail.com>
2025-07-29 23:20:49 +0300

    create seeders

--- server/database/seeders/UserSeeder.php
+++ server/database/seeders/UserSeeder.php
@@ -2,15 +2,15 @@
 
 namespace Database\Seeders;
 
-use App\Models\Product;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class ProductSeeder extends Seeder {
+class UserSeeder extends Seeder {
   /**
    * Run the database seeds.
    */
   public function run(): void {
-    Product::factory()->count(20)->create();
+    User::factory()->count(20)->create();
   }
 }
