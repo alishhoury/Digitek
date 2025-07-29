33a62154488479bf27ccf821cbab7fd72f876ad1
MarounnGhayaa <marounghaya@icloud.com>
2025-07-30 00:22:54 +0300

    Created initial landing page alongside a footer component

--- digitek/src/Pages/LandingPage/index.jsx
+++ digitek/src/Pages/LandingPage/index.jsx
@@ -1,8 +1,48 @@
-const LandingPage = () => {
+import "./style.css";
+import Button from "../../Components/Button";
+import landingImg from "../../assets/TechImg.jpeg";
+import Footer from "../../Components/Footer";
+import { useNavigate } from "react-router-dom";
 
-  return (
-    <div>Test Landing</div>
-  );
+const LandingPage = () => {
+    const navigate = useNavigate();
+    return (
+        <body className="landing-body">
+            <main className="landing-main">
+                <section className="landing-section">
+                    <div className="landing-text">
+                        <h1 className="landing-title">
+                            Power Your Everyday With Smart Accessories
+                        </h1>
+                        <h3 className="landing-subtitle">
+                            Discover what truly matters with personalized choices and a seamless, fulfilling shopping experience.
+                        </h3>
+                    </div>
+                    <div className="landing-buttons">
+                        <Button
+                            text={"Shop Now"}
+                            className="landing-btn-primary"
+                            onClickListener={() => {
+                                navigate("/userPage");
+                            }}
+                        />
+                        <Button
+                            text={"Login"}
+                            className="landing-btn-secondary"
+                            onClickListener={() => {
+                                navigate("/auth");
+                            }}
+                        />
+                    </div>
+                </section>
+
+                <aside className="landing-aside">
+                    <img src={landingImg} alt="Landing Image" className="landing-image"/>
+                </aside>
+            </main>
+            <Footer />
+        </body>
+    );
 };
 
 export default LandingPage;
