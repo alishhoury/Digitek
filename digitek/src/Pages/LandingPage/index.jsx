9c08c1368409c44288fa4046dd6deee121cc8bed
Ali AlSaghir <alisalsaghir@gmail.com>
2025-07-30 17:22:06 +0300

    fix auth

--- digitek/src/Pages/LandingPage/index.jsx
+++ digitek/src/Pages/LandingPage/index.jsx
@@ -5,44 +5,45 @@ import Footer from "../../Components/Footer";
 import { useNavigate } from "react-router-dom";
 
 const LandingPage = () => {
-    const navigate = useNavigate();
-    return (
-        <body className="landing-body">
-            <main className="landing-main">
-                <section className="landing-section">
-                    <div className="landing-text">
-                        <h1 className="landing-title">
-                            Power Your Everyday With Smart Accessories
-                        </h1>
-                        <h3 className="landing-subtitle">
-                            Discover what truly matters with personalized choices and a seamless, fulfilling shopping experience.
-                        </h3>
-                    </div>
-                    <div className="landing-buttons">
-                        <Button
-                            text={"Shop Now"}
-                            className="landing-btn-primary"
-                            onClickListener={() => {
-                                navigate("/home");
-                            }}
-                        />
-                        <Button
-                            text={"Login"}
-                            className="landing-btn-secondary"
-                            onClickListener={() => {
-                                navigate("/auth");
-                            }}
-                        />
-                    </div>
-                </section>
+  const navigate = useNavigate();
+  return (
+    <div className="landing-body">
+      <main className="landing-main">
+        <section className="landing-section">
+          <div className="landing-text">
+            <h1 className="landing-title">
+              Power Your Everyday With Smart Accessories
+            </h1>
+            <h3 className="landing-subtitle">
+              Discover what truly matters with personalized choices and a
+              seamless, fulfilling shopping experience.
+            </h3>
+          </div>
+          <div className="landing-buttons">
+            <Button
+              text={"Shop Now"}
+              className="landing-btn-primary"
+              onClickListener={() => {
+                navigate("/home");
+              }}
+            />
+            <Button
+              text={"Login"}
+              className="landing-btn-secondary"
+              onClickListener={() => {
+                navigate("/auth");
+              }}
+            />
+          </div>
+        </section>
 
-                <aside className="landing-aside">
-                    <img src={landingImg} alt="Landing Image" className="landing-image"/>
-                </aside>
-            </main>
-            <Footer />
-        </body>
-    );
+        <aside className="landing-aside">
+          <img src={landingImg} alt="Landing Image" className="landing-image" />
+        </aside>
+      </main>
+      <Footer />
+    </div>
+  );
 };
 
 export default LandingPage;
