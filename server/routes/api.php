e94be3789e5da2abf2053d9af0823bb5d614e062
Ali AlSaghir <alisalsaghir@gmail.com>
2025-07-30 09:46:49 +0300

    apply auth api's and the middlewares

--- server/routes/api.php
+++ server/routes/api.php
@@ -1,8 +1,23 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Shared\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::group(['prefix' => 'v0.1'], function () {
+
+  Route::group(['prefix' => 'auth'], function () {
+    Route::middleware('custom.guest')->group(function () {
+      Route::post('login', [AuthController::class, 'login']);
+      Route::post('register', [AuthController::class, 'register']);
+    });
+
+    Route::middleware('cookie.auth')->group(function () {
+      Route::post('logout', [AuthController::class, 'logout']);
+      Route::post('me', [AuthController::class, 'me']);
+    });
+  });
+});
+
+
+Route::middleware('role:admin')->group(function () {
+});
