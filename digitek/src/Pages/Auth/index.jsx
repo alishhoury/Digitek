52f14c0dbb0b0199c0d511de7386c1781dedc263
MarounnGhayaa <marounghaya@icloud.com>
2025-07-30 11:45:50 +0300

    Created auth page with login/register forms components

--- digitek/src/Pages/Auth/index.jsx
+++ digitek/src/Pages/Auth/index.jsx
@@ -1,8 +1,26 @@
-const Auth = () => {
+import "./style.css";
+import { useState } from "react";
+import LoginForm from "../../Components/Content/Auth/Login";
+import SignUpForm from "../../Components/Content/Auth/Register";
 
-  return (
-    <div>Test Auth</div>
-  );
+const Auth = () => {
+    const [isLogin, setIsLogin] = useState(true);
+    
+    const switchForm = () => {
+        setIsLogin(!isLogin);
+    };
+    
+    return (
+        <div className="auth-page">
+            <div className="auth-box">
+                {isLogin ? (
+                    <LoginForm toggle={switchForm}/>
+                ) : (
+                    <SignUpForm toggle={switchForm}/>
+                )}
+            </div>
+        </div>
+    );
 };
 
 export default Auth;
