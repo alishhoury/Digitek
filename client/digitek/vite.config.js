aba5b26f44173e409904a1ec2645e238cd979288
Ali AlSaghir <alisalsaghir@gmail.com>
2025-08-02 16:27:21 +0300

    fix a bug

--- client/digitek/vite.config.js
+++ client/digitek/vite.config.js
@@ -1,18 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    force: true,
+  },
   server: {
-    host: '0.0.0.0',
+    host: "0.0.0.0",
     port: 5173,
     strictPort: true,
     watch: {
       usePolling: true,
     },
     hmr: {
-      host: 'localhost',
+      host: "localhost",
       port: 5173,
     },
   },
-})
+});
